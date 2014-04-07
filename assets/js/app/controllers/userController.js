bdd8d551bbedf9beee774e124757696582e41b2c
Charlie Thomason <charlie.thomason@razorfish.com>
2014-04-07 15:57:06 -0500

    Added confirm delete modal

--- assets/js/app/controllers/userController.js
+++ assets/js/app/controllers/userController.js
@@ -1,5 +1,18 @@
 App.UserController = Ember.ObjectController.extend({
+	deleteMode: false,
 	actions: {
+		delete: function() {
+			this.toggleProperty('deleteMode');
+		},
+		cancelDelete: function() {
+			this.set('deleteMode', false);
+		},
+		confirmDelete: function() {
+			this.get('model').deleteRecord();
+			this.get('model').save();
+			this.transitionToRoute('users');
+			this.set('deleteMode', false);
+		},
 		edit: function(){
 			this.transitionToRoute('user.edit');
 		}
